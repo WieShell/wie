7da9dc9701fe73e388c5499d1ce2262c1ba6eb7d
Milo <50248166+Milo123459@users.noreply.github.com>
2021-03-13 08:21:48 +0000

    start of exciting stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+use std::io;
 pub fn run() {
-    
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Error! Couldn't read correctly!");
+        println!("{}", input.replace("\n", ""))
+    }
 }
